785480a208a1dc3276cb8e421253a0b0d64289bc
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-10-28 17:55:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const makeChange = (c) => {
   // your name here
+  const change = {};
+  c = Number(c);
+
+  change.q = Math.floor(c / 25);
+  c = c % 25;
+
+  change.d = Math.floor(c / 10);
+  c = c % 10;
+
+  change.n = Math.floor(c / 5);
+  c = c % 5;
+
+  change.p = c;
+
+  return change;
 };
 
 // Do not the change the code below
